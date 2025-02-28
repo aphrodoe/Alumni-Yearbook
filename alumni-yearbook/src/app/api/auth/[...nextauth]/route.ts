94a0248b171624c79e5cd258fafe0892378b75d0
dj-257 <akhildhyani420@gmail.com>
2025-02-28 20:35:50 +0530

    add mongo integration

--- alumni-yearbook/src/app/api/auth/[...nextauth]/route.ts
+++ alumni-yearbook/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import dbConnect from "../../../../lib/mongodb";
+import User from "../../../models/User";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -10,10 +12,28 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-
     async signIn({ user, account, profile }) {
       const email = user.email || "";
-      const isAllowedEmail = email.startsWith("b21");
+      const isAllowedEmail = email.startsWith("b24");
+      
+      if (isAllowedEmail) {
+        await dbConnect();
+        
+        try {
+          const existingUser = await User.findOne({ email: user.email });
+          
+          if (!existingUser) {
+            await User.create({
+              email: user.email,
+              name: user.name,
+            });
+          }
+        } catch (error) {
+          console.error("Error saving user to MongoDB:", error);
+
+        }
+      }
+      
       return isAllowedEmail;
     },
     async session({ session, token }) {
@@ -22,6 +42,9 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
+  },
+  pages: {
+    error: "/auth/error",
   },
 };
 
