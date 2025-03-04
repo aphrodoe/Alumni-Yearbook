1573f68d2ec954398a95bbd107b708c2ca7f070b
dj-257 <akhildhyani420@gmail.com>
2025-03-05 02:46:55 +0530

    Revert "add quote"
    This reverts commit 6824185fcc607f4821529aea31f6d89d47761333.

--- alumni-yearbook/src/app/models/Quote.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-
-const QuoteSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    quote: { type: String, required: true },
-});
-
-export default mongoose.models.Quote || mongoose.model('Quote', QuoteSchema);
