6824185fcc607f4821529aea31f6d89d47761333
dj-257 <akhildhyani420@gmail.com>
2025-03-05 02:26:15 +0530

    add quote

---
+++ alumni-yearbook/src/app/models/Quote.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+
+const QuoteSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    quote: { type: String, required: true },
+});
+
+export default mongoose.models.Quote || mongoose.model('Quote', QuoteSchema);
